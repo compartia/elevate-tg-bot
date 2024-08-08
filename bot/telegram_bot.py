7ecc646c6f8007fc004fb928650fa2c757aa73f4
Artem Zaborskiy <compartia@gmail.com>
2024-08-08 21:44:42 +0800

    Update telegram_bot.py

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -1,17 +1,16 @@
 from __future__ import annotations
 
 import logging
-from uuid import uuid4
 
-from telegram import BotCommand
-from telegram import BotCommandScopeAllGroupChats, Update, constants
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, \
-    filters, Application, ContextTypes
+from telegram import BotCommand, BotCommandScopeAllGroupChats, Update, constants
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, Application, ContextTypes
 
 from ai_helper import AIHelper, localized_text
-from utils import is_group_chat, get_thread_id, message_text, wrap_with_indicator, split_into_chunks, \
-    is_allowed, is_within_budget, \
-    get_reply_to_message_id, add_chat_request_to_usage_tracker, error_handler, is_direct_result, handle_direct_result
+from utils import (
+    is_group_chat, get_thread_id, message_text, wrap_with_indicator, split_into_chunks,
+    is_allowed, is_within_budget, get_reply_to_message_id, add_chat_request_to_usage_tracker,
+    error_handler, is_direct_result, handle_direct_result
+)
 
 
 class ChatGPTTelegramBot:
@@ -21,58 +20,34 @@ class ChatGPTTelegramBot:
 
     def __init__(self, config: dict, openai: AIHelper):
         """
-        Initializes the bot with the given configuration and GPT bot object.
+        Initializes the bot with the given configuration and AI helper object.
         :param config: A dictionary containing the bot configuration
-        :param openai: OpenAIHelper object
+        :param openai: AIHelper object
         """
         self.config = config
         self.openai = openai
         bot_language = self.config['bot_language']
+
+        # Define bot commands
         self.commands = [
-            # BotCommand(command='help', description=localized_text('help_description', bot_language)),
-            BotCommand(command='reset', description=localized_text('reset_description', bot_language)),
-            # BotCommand(command='stats', description=localized_text('stats_description', bot_language)),
-            # BotCommand(command='resend', description=localized_text('resend_description', bot_language))
+            BotCommand(command='reset', description=localized_text('reset_description', bot_language))
         ]
-        # If imaging is enabled, add the "image" command to the list
-        # if self.config.get('enable_image_generation', False):
-        #     self.commands.append(BotCommand(command='image', description=localized_text('image_description', bot_language)))
-        #
-        # if self.config.get('enable_tts_generation', False):
-        #     self.commands.append(BotCommand(command='tts', description=localized_text('tts_description', bot_language)))
+        self.group_commands = [
+                                  BotCommand(command='chat',
+                                             description=localized_text('chat_description', bot_language))
+                              ] + self.commands
 
-        self.group_commands = [BotCommand(
-            command='chat', description=localized_text('chat_description', bot_language)
-        )] + self.commands
-
+        # Set up messages for disallowed users and budget limits
         self.disallowed_message = localized_text('disallowed', bot_language)
         self.budget_limit_message = localized_text('budget_limit', bot_language)
 
+        # Initialize usage tracking
         self.usage = {}
         self.last_message = {}
-        self.inline_queries_cache = {}
-
-    async def help(self, update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-        """
-        Shows the help menu.
-        """
-        commands = self.group_commands if is_group_chat(update) else self.commands
-        commands_description = [f'/{command.command} - {command.description}' for command in commands]
-        bot_language = self.config['bot_language']
-        help_text = (
-                localized_text('help_text', bot_language)[0] +
-                '\n\n' +
-                '\n'.join(commands_description) +
-                '\n\n' +
-                localized_text('help_text', bot_language)[1] +
-                '\n\n' +
-                localized_text('help_text', bot_language)[2]
-        )
-        await update.message.reply_text(help_text, disable_web_page_preview=True)
 
     async def reset(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
-        Resets the conversation.
+        Resets the conversation for the user.
         """
         if not await is_allowed(self.config, update, context):
             logging.warning(f'User {update.message.from_user.name} (id: {update.message.from_user.id}) '
@@ -84,7 +59,6 @@ class ChatGPTTelegramBot:
                      f'(id: {update.message.from_user.id})...')
 
         chat_id = update.effective_chat.id
-        # reset_content = message_text(update.message)
         self.openai.reset_conversation(chat_id=chat_id)
         await update.effective_message.reply_text(
             message_thread_id=get_thread_id(update),
@@ -92,16 +66,17 @@ class ChatGPTTelegramBot:
         )
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        # update.message='\n'
-        # prompt = '😂'  # TODO: tranduire
-        prompt = 'Привет'  # TODO: tranduire
+        """
+        Handles the /start command.
+        """
+        prompt = 'Привет'  # TODO: Translate this
         await self.__prompt(prompt=prompt, chat_id=update.effective_chat.id, user_id=update.message.from_user.id,
                             update=update, context=context)
 
     async def prompt(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """
-                React to incoming messages and respond accordingly.
-                """
+        Handles incoming messages and generates responses.
+        """
         if update.edited_message or not update.message or update.message.via_bot:
             return
 
@@ -120,7 +95,9 @@ class ChatGPTTelegramBot:
 
     async def __prompt(self, prompt: str, chat_id: int, user_id: int, update: Update,
                        context: ContextTypes.DEFAULT_TYPE):
-
+        """
+        Internal method to process the prompt and generate a response.
+        """
         if is_group_chat(update):
             trigger_keyword = self.config['group_trigger_keyword']
 
@@ -149,28 +126,22 @@ class ChatGPTTelegramBot:
                 if is_direct_result(response):
                     return await handle_direct_result(self.config, update, response)
 
-                # Split into chunks of 4096 characters (Telegram's message limit)
                 chunks = split_into_chunks(response)
 
                 for index, chunk in enumerate(chunks):
                     try:
                         await update.effective_message.reply_text(
                             message_thread_id=get_thread_id(update),
-                            reply_to_message_id=get_reply_to_message_id(self.config,
-                                                                        update) if index == 0 else None,
+                            reply_to_message_id=get_reply_to_message_id(self.config, update) if index == 0 else None,
                             text=chunk,
                             parse_mode=constants.ParseMode.MARKDOWN
                         )
                     except Exception:
-                        try:
-                            await update.effective_message.reply_text(
-                                message_thread_id=get_thread_id(update),
-                                reply_to_message_id=get_reply_to_message_id(self.config,
-                                                                            update) if index == 0 else None,
-                                text=chunk
-                            )
-                        except Exception as exception:
-                            raise exception
+                        await update.effective_message.reply_text(
+                            message_thread_id=get_thread_id(update),
+                            reply_to_message_id=get_reply_to_message_id(self.config, update) if index == 0 else None,
+                            text=chunk
+                        )
 
             await wrap_with_indicator(update, context, _reply, constants.ChatAction.TYPING)
 
@@ -185,55 +156,42 @@ class ChatGPTTelegramBot:
                 parse_mode=constants.ParseMode.MARKDOWN
             )
 
-    async def check_allowed_and_within_budget(self, update: Update, context: ContextTypes.DEFAULT_TYPE,
-                                              is_inline=False) -> bool:
+    async def check_allowed_and_within_budget(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> bool:
         """
-        Checks if the user is allowed to use the bot and if they are within their budget
-        :param update: Telegram update object
-        :param context: Telegram context object
-        :param is_inline: Boolean flag for inline queries
-        :return: Boolean indicating if the user is allowed to use the bot
+        Checks if the user is allowed to use the bot and is within their budget.
         """
-        name = update.inline_query.from_user.name if is_inline else update.message.from_user.name
-        user_id = update.inline_query.from_user.id if is_inline else update.message.from_user.id
+        name = update.message.from_user.name
+        user_id = update.message.from_user.id
 
-        if not await is_allowed(self.config, update, context, is_inline=is_inline):
+        if not await is_allowed(self.config, update, context):
             logging.warning(f'User {name} (id: {user_id}) is not allowed to use the bot')
-            await self.send_disallowed_message(update, context, is_inline)
+            await self.send_disallowed_message(update, context)
             return False
-        if not is_within_budget(self.config, self.usage, update, is_inline=is_inline):
+        if not is_within_budget(self.config, self.usage, update):
             logging.warning(f'User {name} (id: {user_id}) reached their usage limit')
-            await self.send_budget_reached_message(update, context, is_inline)
+            await self.send_budget_reached_message(update, context)
             return False
 
         return True
 
-    async def send_disallowed_message(self, update: Update, _: ContextTypes.DEFAULT_TYPE, is_inline=False):
+    async def send_disallowed_message(self, update: Update, _: ContextTypes.DEFAULT_TYPE):
         """
         Sends the disallowed message to the user.
         """
-        if not is_inline:
-            await update.effective_message.reply_text(
-                message_thread_id=get_thread_id(update),
-                text=self.disallowed_message,
-                disable_web_page_preview=True
-            )
-        else:
-            result_id = str(uuid4())
-            await self.send_inline_query_result(update, result_id, message_content=self.disallowed_message)
+        await update.effective_message.reply_text(
+            message_thread_id=get_thread_id(update),
+            text=self.disallowed_message,
+            disable_web_page_preview=True
+        )
 
-    async def send_budget_reached_message(self, update: Update, _: ContextTypes.DEFAULT_TYPE, is_inline=False):
+    async def send_budget_reached_message(self, update: Update, _: ContextTypes.DEFAULT_TYPE):
         """
         Sends the budget reached message to the user.
         """
-        if not is_inline:
-            await update.effective_message.reply_text(
-                message_thread_id=get_thread_id(update),
-                text=self.budget_limit_message
-            )
-        else:
-            result_id = str(uuid4())
-            await self.send_inline_query_result(update, result_id, message_content=self.budget_limit_message)
+        await update.effective_message.reply_text(
+            message_thread_id=get_thread_id(update),
+            text=self.budget_limit_message
+        )
 
     async def post_init(self, application: Application) -> None:
         """
@@ -255,26 +213,10 @@ class ChatGPTTelegramBot:
             .build()
 
         application.add_handler(CommandHandler('reset', self.reset))
-        # application.add_handler(CommandHandler('help', self.help))
-
         application.add_handler(CommandHandler('start', self.start))
-
-        # application.add_handler(CommandHandler('stats', self.stats))
-        # application.add_handler(CommandHandler('resend', self.resend))
-
-        application.add_handler(CommandHandler(
-            'chat', self.prompt, filters=filters.ChatType.GROUP | filters.ChatType.SUPERGROUP)
-        )
-
-        # application.add_handler(MessageHandler(
-        #     filters.AUDIO | filters.VOICE | filters.Document.AUDIO,
-        #     self.transcribe))
-
+        application.add_handler(
+            CommandHandler('chat', self.prompt, filters=filters.ChatType.GROUP | filters.ChatType.SUPERGROUP))
         application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self.prompt))
-        # application.add_handler(InlineQueryHandler(self.inline_query, chat_types=[
-        #     constants.ChatType.GROUP, constants.ChatType.SUPERGROUP, constants.ChatType.PRIVATE
-        # ]))
-        # application.add_handler(CallbackQueryHandler(self.handle_callback_inline_query))
 
         application.add_error_handler(error_handler)
 
